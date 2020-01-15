9b16d7422a63a0561d236e19042aec3121761b9c
James Ge <james.ge1@outlook.com>
2020-01-15 11:44:25 -0800

    initial commit

---
+++ myApp.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+app.get("/api/timestamp", function(req, res){
+    var date = new Date();
+    res.json({utc: date.toUTCString(), unix: Math.floor(date/1000)})
+})
+
+app.get("/api/timestamp/:date_string?", function(req, res){
+    var dateString = req.params.date_string
+    var epoch, date= new Date(0);
+    console.log(dateString)
+    if (!isNaN(dateString)){
+        epoch = parseInt(dateString);
+        date.setUTCSeconds(Math.floor(epoch)/1000)
+        res.json({unix: epoch, utc: date.toUTCString()})
+    } else if (dateString.length === 10){
+        epoch = Date.parse(dateString)
+        if (epoch){
+            date.setUTCSeconds(Math.floor(epoch)/1000)
+            res.json({unix: epoch, utc: date.toUTCString()})
+        } else {
+            res.send("something bad")
+        }
+    }
+})
+
+module.exports = app;
